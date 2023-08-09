b2cc1bb777d098a3427d64c3c0703b0151a82daf
Davide Di Palma <davidewebfullstack@gmail.com>
2023-08-09 17:06:41 +0200

    Start

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,41 @@
+<nav class="fixed-top navbar navbar-expand-lg bg-navbar my-violet">
+    <div class="container-fluid">
+      <a class="nav-font my-violet navbar-brand" href="#"><i class="icon-size fa-solid fa-headphones fa-fade fa-4x"></i></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+        </span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item me-4">
+            <a class="my-violet nav-font nav-link fs-3" aria-current="page" href="#">Home Page</a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="my-violet nav-font nav-link fs-3" href="#">Artists</a>
+          </li>
+          <li class="nav-item me-4">
+                <a class="my-violet nav-font nav-link fs-3" href="#">Playlist</a>
+          </li>
+          <li class=" nav-item dropdown me-4">
+            <a class="fs-3 my-violet nav-font nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              User
+            </a>
+            <ul class="dropdown-menu bg-navbar">
+              <li><a class="my-violet nav-font dropdown-item" href="#">Add New File</a></li>
+              <li><a class="my-violet nav-font dropdown-item" href="#">Add New Playlist</a></li>
+              <li><hr class="my-violet nav-font dropdown-divider"></li>
+              <li><a class="my-violet nav-font dropdown-item" href="#">LogOut</a></li>
+            </ul>
+          </li>
+          <li class="nav-item me-4">
+            <a class="my-violet nav-font nav-link fs-3 " href="#">Contacts</a>
+          </li>
+        </ul>
+        <div class="nav-item pe-5 me-5">
+            <span class="nav-item  fs-3 ">
+                <a class="my-violet nav-font nav-link fs-3" href="#">Accedi</a>
+            </span>
+        </div>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<x-layout>
+    <div class="container">
+        <div class="row justify-content-end">
+            <div class="col-12">
+                <h1 class="display-1 mt-5 pt-5 my-violet">Trap Base</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="pt-5 d-flex justify-content-end col-3 col-md-4">
+                <i class="ps-5 ms-5 my-violet fa-sharp fa-solid fa-music fa-beat-fade fa-5x"></i>
+            </div>
+            <div class="col-12 col-md-8">
+                <h3 class=" display-3 ms-5 my-5 my-violet">Close Your Eyse</h3>
+                <p class="my-violet display-5"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi repudiandae cum cupiditate quibusdam. Maiores, labore! Est quam itaque vel sapiente beatae aspernatur omnis incidunt nam!</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container media-player">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                
+            </div>
+        </div>
+    </div>
+</x-layout>
